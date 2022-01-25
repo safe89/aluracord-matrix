88c105bf225e88bdd3ae6e4fda8fcc685eecc1f1
Sara Fernandes <43149910+safe89@users.noreply.github.com>
2022-01-25 22:04:39 +0000

    Matrix Chat - criando o nosso projeto (aula 2 | Imersão React 4)

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image} from '@skynexui/components'
-
-function GlobalStyle() {
-    return(
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-            }
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-            /* App fit Height */
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex: 1;
-            }
-            #__next > * {
-                flex: 1;
-            }
-            /* ./App fit Height */
-    `}</style>
-    );
-}
+import React from 'react';
+import { useRouter } from 'next/router'
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -61,11 +34,15 @@ function Titulo(props) {
   export default HomePage*/
 
   export default function PaginaInicial() {
-    const username = 'safe89';
+    //const username = 'safe89';
+    const [username, setUsername] = React.useState('safe89');
+    const roteamento = useRouter();
+    const disableButton = username.length <= 2 ? true : false;
+
+    console.log(disableButton);
 
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +70,13 @@ function Titulo(props) {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (event) {
+                event.preventDefault();
+                console.log('Form submetido!')
+                roteamento.push('/chat')
+                // A forma abaixo é a convencional de JS e não REact
+                //window.location.href = '/chat'
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px'
@@ -104,6 +88,15 @@ function Titulo(props) {
               </Text>
 
               <TextField
+                value={username}
+                onChange={function (event) {
+                  console.log('digitando...', event.target.value);
+                  // Onde está o valor?
+                  const valor = event.target.value;
+                  // Trocar valor da variável
+                  // através do React que avisa quem precisa
+                  setUsername(valor);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -114,9 +107,23 @@ function Titulo(props) {
                   },
                 }}
               />
+
+              {/* <input
+                type="text"
+                value={username}
+                onChange={function (event) {
+                  console.log('digitando...', event.target.value);
+                  // Onde está o valor?
+                  const valor = event.target.value;
+                  // Trocar valor da variável
+                  // através do React que avisa quem precisa
+                  setUsername(valor);
+                }}>
+              </input> */}
               <Button
                 type='submit'
                 label='Entrar'
+                disabled = {disableButton}
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["100"],
